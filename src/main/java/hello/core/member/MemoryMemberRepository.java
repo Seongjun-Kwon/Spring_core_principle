5256bc0de5cea6056f570d3beceed1f04a1a8d27
Kwon seong jun <tjdwns1865@naver.com>
2021-08-21 21:31:19 +0900

    spring_core_principle

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -3,13 +3,13 @@ package hello.core.member;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MemoryMemberRepository implements MemberRepository{
+public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
 
     @Override
     public void save(Member member) {
-        store.put(member.getMemberId(),member);
+        store.put(member.getMemberId(), member);
     }
 
     @Override
